5e6f7fe33c28d81f20ad1b993ddc9104decc902d
Ryan Joya <ryanjoya@users.noreply.github.com>
2015-01-24 21:52:52 -0600

    Create 1.9 Multi-Line Comments.py

---
+++ 1.9 Multi-Line Comments.py
@@ -0,0 +1,2 @@
+"""I miss the old no life Kripp. It's a shame a game like Hearthstone has turned him from the no life all day "hardcore gamer" to someone with a life and a girlfriend with tons of money. Kripp I will now unsub to you and pray for you that one day you will come back to what you once were and not the $$ making girl kissing gamer with a life, that is all.
+"""
